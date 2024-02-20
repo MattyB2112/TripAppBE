10fdcf0d8e04e1cecd5a46ed7e08c06c4cd65a64
jgroma <jgromadzinska9@gmail.com>
2024-02-20 11:54:03 +0000

    Add data to test database

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -9,7 +9,22 @@ const seedUsers = [
   {
     username: "Justyna",
     password: "password2",
-    email: "jsutyna@justyna.com",
+    email: "justyna@justyna.com",
+  },
+  {
+    username: "Lala",
+    password: "password3",
+    email: "lala@lala.com",
+  },
+  {
+    username: "Jack",
+    password: "password4",
+    email: "jack@jack.com",
+  },
+  {
+    username: "Stavros",
+    password: "password5",
+    email: "stavros@stavros.com",
   },
 ];
 
@@ -28,10 +43,56 @@ const seedTrips = [
       {
         username: "Justyna",
         password: "password2",
-        email: "jsutyna@justyna.com",
+        email: "justyna@justyna.com",
       },
     ],
+    activities: [
+      "museum", "restaurant", "club"
+    ]
   },
+  {
+    name: "Malta",
+    admin: "Stavros",
+    members: [{
+      username: "Stavros",
+      password: "password5",
+      email: "stavros@stavros.com",
+    },]
+  },
+  {
+    name: "Group trip",
+    admin: "Jack",
+    members: [
+      {
+        username: "MattB",
+        password: "password",
+        email: "mattb@matt.com",
+      },
+      {
+        username: "Justyna",
+        password: "password2",
+        email: "justyna@justyna.com",
+      },
+      {
+        username: "Lala",
+        password: "password3",
+        email: "lala@lala.com",
+      },
+      {
+        username: "Jack",
+        password: "password4",
+        email: "jack@jack.com",
+      },
+      {
+        username: "Stavros",
+        password: "password5",
+        email: "stavros@stavros.com",
+      },
+    ],
+    activities: ["theatre", "golfing"],
+    travel: ["plane at 1pm", "taxi at 6pm"],
+    stay: ["Random Inn"],
+  }
 ];
 
 const seedDB = async () => {
