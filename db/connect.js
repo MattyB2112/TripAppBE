72a97388a8dd0b4081d4866c22b92267e90e649d
Matt Barker <mattbarker23@hotmail.com>
2024-02-26 09:13:18 +0000

    fixed .env variable not being read issue

--- db/connect.js
+++ db/connect.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose");
-mongoose.set("strictQuery", false);
 
-const URI =
-  "mongodb+srv://mattbarker23:TRIPAPPBACKEND@cluster0.jj43bd7.mongodb.net/test";
+mongoose.set("strictQuery", false);
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MONGODB connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
