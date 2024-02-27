211b5c6633c72e4bdaacd71f03091f36331f8c13
Matt Barker <mattbarker23@hotmail.com>
2024-02-27 13:59:37 +0000

    changed node start script

--- listen.js
+++ listen.js
@@ -1,4 +1,7 @@
+const connectDB = require("./db/connect");
 const app = require("./app");
+
+connectDB();
 const PORT = process.env.PORT || 9090;
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}...`));
